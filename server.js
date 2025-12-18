ac3241ac58ead90cbd707e3a3fe3223a60e75e22
Selorm Ayornu <95534257+yoselorm@users.noreply.github.com>
2025-12-18 16:37:46 +0000

    fix: cookie

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(cookieParser());
 
 app.use(cors({
     origin: [
-      "http://localhost:3000",
       "http://localhost:5173",
       "http://localhost:5174",
       "http://127.0.0.1:3000",
